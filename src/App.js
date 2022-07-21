09832788694b8f76c71227289413a8b8652cb30b
oussamaosman02 <109099115+Oussamaosman02@users.noreply.github.com>
2022-07-21 15:27:36 +0200

    adding tags

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-import './App.css';
+import "./App.css";
+import Lista from "./components/Lista";
 
 function App() {
   return (
     <div className="App">
-     
+     <Lista/>
+     <Lista/>
+     <Lista/>
+     <Lista/>
+     <Lista/>
+     <Lista/>
+     <Lista/>
+     <Lista/>
     </div>
   );
 }
